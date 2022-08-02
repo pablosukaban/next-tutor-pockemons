36872d5aa9149ea7dbc02308afe402c880a9f5e8
ilya <zapaska96@gmail.com>
2022-08-02 16:38:51 +0500

    client side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link';
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    
-    async function getPoks(){
-      const resp = await fetch("https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json")
-      setPokemons(await resp.json())
+    async function getPoks() {
+      const resp = await fetch(
+        "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+      );
+      setPokemons(await resp.json());
     }
 
-    getPoks()
-  }, [])
+    getPoks();
+  }, []);
 
   return (
     <div>
+      <Head>
+        <title>Pokemons app</title>
+      </Head>
       <h1>yo {pokemons.length}</h1>
       <div className={styles.grid}>
-        {pokemons.map(pokemon => (
+        {pokemons.map((pokemon) => (
           <div className={styles.card} key={pokemon.id}>
-            <Link href={`/pokemon/${pokemon.id}`} >
+            <Link href={`/pokemon/${pokemon.id}`}>
               <a>
-                <img src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`} alt={pokemon.name}/>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
                 <h3>{pokemon.name}</h3>
               </a>
             </Link>
@@ -33,5 +40,5 @@ export default function Home() {
         ))}
       </div>
     </div>
-  )
+  );
 }
--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,8 +1,69 @@
-import { useRouter } from "next/router"
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "../../styles/Details.module.css";
 
-export default function Details(){
-  const {query: {id}} = useRouter()
+export default function Details() {
+  const {
+    query: { id },
+  } = useRouter();
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    async function getPoke() {
+      const resp = await fetch(
+        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+      );
+      setPokemon(await resp.json());
+    }
+    if (id) {
+      getPoke();
+    }
+  }, [id]);
+
+  if (!pokemon) {
+    return <h1>No pokemons?</h1>;
+  }
   return (
-    <h1>Hi {id}</h1>
-  )
+    <div>
+      <Head>
+        <title>{pokemon.name}</title>
+      </Head>
+      <div>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </div>
+      <div className={styles.layout}>
+        <div>
+          <img
+            className={styles.picture}
+            src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+            alt={pokemon.name.english}
+          />
+        </div>
+        <div>
+          <div className={styles.name}>{pokemon.name}</div>
+          <div className={styles.type}>{pokemon.type.join(", ")}</div>
+          <table>
+            <thead className={styles.header}>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon.stats.map(({ name, value }) => (
+                <tr key={name}>
+                  <td className={styles.attribute}>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
